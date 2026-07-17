612656979bb3c289d373908220c2923136545618
judahtreeventures <judahtreeventures@gmail.com>
2026-07-17 16:50:45 -0500

    Remove duplicate refund index migration

--- supabase/migrations/20260717213308_d2f14947-6c36-479c-8d2d-87e0a5738aba.sql
+++
@@ -1,6 +0,0 @@
-DROP INDEX IF EXISTS public.shekel_ledger_stripe_session_unique;
-
-CREATE UNIQUE INDEX shekel_ledger_stripe_session_unique
-  ON public.shekel_ledger (stripe_session_id)
-  WHERE stripe_session_id IS NOT NULL
-    AND kind <> 'bundle_refund';
